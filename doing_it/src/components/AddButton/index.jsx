d89a28095b1f247b3163d63e8de5795dc3f3628b
jillwilliams <codeGeek.03.14@gmail.com>
2019-04-21 13:06:58 -0700

    adding photos and making the page responsive

--- doing_it/src/components/AddButton/index.jsx
+++ doing_it/src/components/AddButton/index.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 
-var count = document.getElementById("itemCountBox").value;
-var addOne = count + 1;
-document.getElementById("addItemToCart").addEventListener("click", addItemToCart);
+
+// WHEN THE + BOX GETS CLICKED, THE WINDOW SHOWS AN INCREASE
+// OF +1 FOR EACH CLICK
+const addTocart = ( ) => {
+    var count = document.getElementById("itemCountBox").value;
+    document.getElementById("addItemToCart").addEventListener("click", 
+        addItemToCart);
+    var count = increaseByOne = ( ) => {
+        while (i = 0; i < 6; i++) {
+        let count = i;
+        };
+    addOne = count + 1;
+    };
 const addItemToCart = ( ) => {
-    document.getElementById("itemCountBox").innerHTML= addOne;
+    document.getElementById("itemCountBox").innerHTML= addToCart( );
+    document.getElementById("itemCountBox").innerHTML= addOne( );
 };
-var subtractOne = count - 1;
-document.getElementById("subtractItemFromCart").addEventListener("click", subtractItemFromCart);
+
+// WHEN THE SUBTRACT BUTTON IS CLICKED, THE ITEM COUNT BOX WILL
+// DECREASE BY ONE EACH TIME THE BUTTON IS CLICKED
+const subtractFromCart = ( )=> {
+    document.getElementById("itemCountBox");
+    document.getElementById("subtractItemFromCart").addEventListener("click", 
+        subtractItemFromCart);
+    var subtractOne = count - 1;
+
 const subtractItemFromCart = ( ) => {
-    document.getElementById("itemCountBox").innerHTML= subtractOne;
+        document.getElementById("itemCountBox").innerHTML= subtractFromCart( );
+        document.getElementById("itemCountBox").innerHTML= subtractOne( );
 };
